b2061a3db47b796360559c506279881b39e51bd2
From Zero <jorge@fromzero.io>
2022-01-03 13:18:54 -0500

    settings fix

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 import os
 
-PORT = os.environ["PORT"]
-SECRET_KEY = os.environ["SECRET_KEY"]
 FLASK_ENV = os.environ["FLASK_ENV"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 MONGODB_HOST = os.environ["MONGODB_HOST"]
 MONGODB_DB = os.environ["MONGODB_DB"]
